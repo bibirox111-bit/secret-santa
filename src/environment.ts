bdc2269f32dc5081522f17972450a3944f80e5cc
pakalina <trollhard6@gmail.com>
2026-01-21 22:45:06 +0100

    placeholders

--- src/environment.ts
+++ src/environment.ts
@@ -1,12 +1,12 @@
 export const environment = {
   firebase: {
-    apiKey: "AIzaSyAdL7x0U5mglrANVYJzLsiVgWtAN14b3Q4",
+    apiKey: "<PLACEHOLDER>",
     authDomain: "secretsanta-d141b.firebaseapp.com",
-    projectId: "secretsanta-d141b",
-    storageBucket: "secretsanta-d141b.firebasestorage.app",
-    messagingSenderId: "491859891403",
-    appId: "1:491859891403:web:6d5f20163a883d9a86b74e",
-    measurementId: "G-QDS8YE2NLX"
+    projectId: "<PLACEHOLDER>",
+    storageBucket: "<PLACEHOLDER>",
+    messagingSenderId: "<PLACEHOLDER>",
+    appId: "<PLACEHOLDER>",
+    measurementId: "<PLACEHOLDER>"
   },
   production: false
 };
